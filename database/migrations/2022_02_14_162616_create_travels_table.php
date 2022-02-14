c54f1527bb5b53c18d3266c325ef2a2c035405e5
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2022-02-14 18:12:11 +0100

    migration completed

---
+++ database/migrations/2022_02_14_162616_create_travels_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            $table->id();
+            $table->string('departure_city', 20);
+            $table->string('arrival_city', 20);
+            $table->string('hotel_name', 20);
+            $table->tinyInteger('hotel_stars')->unsigned();
+            $table->decimal('price', 6, 2);
+            $table->text('description')->nullable();
+            $table->string('first_name', 20);
+            $table->string('last_name', 20);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travels');
+    }
+}
